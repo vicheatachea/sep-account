0b9a50aa96b52ea86d51779f79a3ced40df2c811
vicheatachea <vicheata.chea@metropolia.fi>
2025-02-24 16:19:06 +0200

    account

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,34 @@
+import org.example.Account;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+    Account account = new Account();
+
+    @Test
+    void testDeposit() {
+        account.deposit(100);
+        assertEquals(100, account.getBalance());
+        account.deposit(200);
+        assertEquals(300, account.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        account.deposit(100);
+        account.withdraw(50);
+        assertEquals(50, account.getBalance());
+        account.withdraw(25);
+        assertEquals(25, account.getBalance());
+    }
+
+    @Test
+    void testMain() {
+        Account account = new Account();
+        account.deposit(100);
+        assertEquals(100, account.getBalance());
+        account.withdraw(50);
+        assertEquals(50, account.getBalance());
+    }
+
+}
